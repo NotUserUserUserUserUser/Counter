ab0db9fe6b8149aee413673ea2950e1afea26955
NotUserUserUserUserUser <112754632+NotUserUserUserUserUser@users.noreply.github.com>
2022-09-04 14:36:36 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+﻿import ScoreboardView from "./scoreboard/ScoreboardView.js";
+
+let playerOneScore = 0;
+let playerTwoScore = 0;
+const root = document.querySelector("#app");
+const view = new ScoreboardView(root, "Player One", "Player Two");
+
+
